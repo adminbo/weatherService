2275b5afaaee74d6b82a7a0815c28b9c864489c3
adminbo <296472653@qq.com>
2017-10-26 12:05:12 +0800

    1

--- freemaker/src/main/java/com/springmvc/framework/controller/Hello.java
+++ freemaker/src/main/java/com/springmvc/framework/controller/Hello.java
@@ -1,5 +1,10 @@
 package com.springmvc.framework.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +16,27 @@ public class Hello {
 	public String hello(Model model){
 		model.addAttribute("obj","framework");
 		return "hello";
+	}
+	@RequestMapping("/list")
+	public String list(Model model){
+		List<String> list = new ArrayList<>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		model.addAttribute("obj",list);
+		return "list";
+	}
+	@RequestMapping("/map")
+	public String map(Model model){
+		Map<String,String> map = new HashMap<>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		map.put("key3", "value3");
+		map.put("key4", "value4");
+		map.put("key5", "value5");
+		model.addAttribute("obj",map);
+		return "map";
 	}
 }
