83379bc2efb6704b9036b7695a0beef8ad3a7768
adminbo <296472653@qq.com>
2017-11-21 13:35:17 +0800

    spring event demo初次导入

---
+++ springEvent/src/main/java/org/spring/event/demo/SpringEventApplication.java
@@ -0,0 +1,12 @@
+package org.spring.event.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventApplication.class, args);
+	}
+}
